09ec8a7767808dafdb8d70ce3b7a65751cfa33ac
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2020-07-12 15:56:16 -0700

    Initial Commit

---
+++ tipc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipc
+//
+//  Created by Sandy S Camilo on 7/12/20.
+//  Copyright © 2020 CodepathSC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
